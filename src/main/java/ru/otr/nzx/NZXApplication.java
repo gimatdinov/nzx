faa0e6f3e724fab133e3e1337b320e5e3062817a
Ильдар <>
2016-11-15 20:31:08 +0300

    quick fix

--- src/main/java/ru/otr/nzx/NZXApplication.java
+++ src/main/java/ru/otr/nzx/NZXApplication.java
@@ -25,47 +25,44 @@ import ru.otr.nzx.config.NZXConfig;
 
 @SpringBootApplication
 public class NZXApplication implements CommandLineRunner {
-	private static Logger log = LoggerFactory.getLogger(NZXApplication.class);
+    private static Logger log = LoggerFactory.getLogger(NZXApplication.class);
 
-	private NZX nzx;
+    private NZX nzx;
 
-	@PreDestroy
-	private void fina() {
-		if (nzx != null) {
-			nzx.stop();
-		}
-	}
+    @PreDestroy
+    private void fina() {
+        if (nzx != null) {
+            nzx.stop();
+        }
+    }
 
-	@Override
-	public void run(String... args) {
-		Options options = new Options();
-		Option nameOption = Option.builder("n").longOpt("name").required(true).numberOfArgs(1).desc("Server name").build();
-		Option configOption = Option.builder("c").longOpt("config").required(true).numberOfArgs(1).desc("Path to configuration file").build();
-		options.addOption(nameOption);
-		options.addOption(configOption);
-		HelpFormatter formatter = new HelpFormatter();
-		CommandLineParser parser = new DefaultParser();
-		try {
-			CommandLine cmdLine = parser.parse(options, args);
-			if (cmdLine.hasOption(nameOption.getOpt()) && cmdLine.hasOption(configOption.getOpt())) {
-				String serverName = cmdLine.getOptionValue("name");
-				File configFile = new File(cmdLine.getOptionValue("config")).getAbsoluteFile();
-				Tracer tracer = new LogbackTracer(serverName);
-				tracer.debug("Config.File", configFile.getPath());
-				nzx = new NZX(new NZXConfig(configFile), tracer);
-				nzx.bootstrap();
-				nzx.start();
-			} else {
-				formatter.printHelp("java [-Dlogging.config=logback.xml] -jar nzx.jar", options);
-			}
-		} catch (ParseException | URISyntaxException e) {
-			log.error(e.getMessage());
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-	}
+    @Override
+    public void run(String... args) {
+        Options options = new Options();
+        Option nameOption = Option.builder("n").longOpt("name").required(true).numberOfArgs(1).desc("Server name").build();
+        Option configOption = Option.builder("c").longOpt("config").required(true).numberOfArgs(1).desc("Path to configuration file").build();
+        options.addOption(nameOption);
+        options.addOption(configOption);
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmdLine = parser.parse(options, args);
+            String serverName = cmdLine.getOptionValue("name");
+            File configFile = new File(cmdLine.getOptionValue("config")).getAbsoluteFile();
+            Tracer tracer = new LogbackTracer(serverName);
+            tracer.debug("Config.File", configFile.getPath());
+            nzx = new NZX(new NZXConfig(configFile), tracer);
+            nzx.bootstrap();
+            nzx.start();
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(NZXApplication.class, args);
-	}
+        } catch (ParseException e) {
+            formatter.printHelp("java [-Dlogging.config=logback.xml] -jar nzx.jar", options);
+        } catch (IOException | URISyntaxException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(NZXApplication.class, args);
+    }
 }
